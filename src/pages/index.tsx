6ac831d9435ba869ae46e75da7e7b41135a6d93e
mimototo <mail@mimototo.jp>
2023-05-17 22:15:19 +0900

    feat: 🎸 tailwind
    add tailwind and header components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import Header from "./componets/Header";
+import HeaderBar from "./componets/HeaderBar";
 
 export default function Home() {
+  const links = [
+    { link: "/about", label: "About" },
+    { link: "/works", label: "Works" },
+    { link: "/blog", label: "Blog" },
+    { link: "/contact", label: "Contact" },
+  ];
   return (
     <div>
-      <Header />
+      <HeaderBar links={links} />
     </div>
   );
 }
