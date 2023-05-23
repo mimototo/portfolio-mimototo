9db94e1ab44d20707f256d0cf55f652711ab5dde
mimototo <mail@mimototo.jp>
2023-05-23 22:03:36 +0900

    feat: 🎸 text icon
    adjust icon and use mantine text

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ToggleTheme from "./ToggleTheme";
-import { Avatar } from "@mantine/core";
+import { Avatar, Text } from "@mantine/core";
 import { useTheme } from "next-themes";
 
 const Header = () => {
@@ -10,10 +10,14 @@ const Header = () => {
     <header className="space-y-4 p-4 sm:px-8 sm:py-4 lg:p-4 xl:px-8 xl:py-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <Avatar src={theme === "light" ? "1.png" : "2.png"} alt="icon" />
-          <h2 className="font-semibold text-gray-800 dark:text-white text-xl">
+          <Avatar
+            src={theme === "light" ? "1.png" : "2.png"}
+            size="lg"
+            alt="icon"
+          />
+          <Text fz="xl" fw={500}>
             mimototo
-          </h2>
+          </Text>
         </div>
         <span className="group inline-flex items-center text-xl font-medium pl-2 pr-3 py-2">
           <ToggleTheme />
